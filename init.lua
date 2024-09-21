861742e4899e90b96890f332a3dd4203f0f0fe44
umaromz <umarmohammed090@gmail.com>
2024-09-21 03:40:51 +0100

    update init.lua to add solarized seperated

--- init.lua
+++ init.lua
@@ -625,7 +625,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         ts_ls = {},
-               --
+        --
 
         lua_ls = {
           -- cmd = {...},
@@ -833,58 +833,28 @@ require('lazy').setup({
     end,
   },
 
-  -- NOTE: CHANGE COLORSCHEME TO SOLARIZED
-  --
-  --
   --
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'lifepillar/vim-solarized8',
-    -- 'folke/tokyonight.nvim',
+    'folke/tokyonight.nvim',
+    --     vim.cmd.colorscheme 'solarized8'
+    --     vim.o.background = 'light'
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'solarized8'
-      vim.o.background = 'light'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-  -- NOTE: CHANGE COLORSCHEME TO SOLARIZED END
-  --
-  --
-  --
-  --
-  --NOTE: DEFAULT COLORSCHEME
-  --
-  -- { -- You can easily change to a different colorscheme.
-  --   -- Change the name of the colorscheme plugin below, and then
-  --   -- change the command in the config to whatever the name of that colorscheme is.
-  --   --
-  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --   'folke/tokyonight.nvim',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   init = function()
-  --     -- Load the colorscheme here.
-  --     -- Like many other themes, this one has different styles, and you could load
-  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --     vim.cmd.colorscheme 'tokyonight-night'
-  --
-  --     -- You can configure highlights by doing something like:
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
-  --NOTE: DEFAULT COLORSCHEME END
-  --
-  --
-  --
-  --
+  -- NOTE: ADD SOLARIZED COLORSCHEME
+  { 'lifepillar/vim-solarized8' },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
